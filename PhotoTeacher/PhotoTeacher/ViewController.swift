931364a891a780b6c8ca60c00572de370ec4288c
rodscarvalho <barretocarvalho.contato@gmail.com>
2020-04-08 15:38:16 -0300

    Criação do Projeto
    Criação do projeto no Xcode

---
+++ PhotoTeacher/PhotoTeacher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTeacher
+//
+//  Created by Rodrigo Barreto de Carvalho on 08/04/20.
+//  Copyright © 2020 Grupo 5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoTeacher/PhotoTeacherTests/PhotoTeacherTests.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoTeacherTests.swift
+//  PhotoTeacherTests
+//
+//  Created by Rodrigo Barreto de Carvalho on 08/04/20.
+//  Copyright © 2020 Grupo 5. All rights reserved.
+//
+
+import XCTest
+@testable import PhotoTeacher
+
+class PhotoTeacherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
